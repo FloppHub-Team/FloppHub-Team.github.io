5172f890974682deabd8dfcec12b7602bd66b2dc
FloppHub Team <non.time666@gmail.com>
2025-12-22 00:35:29 -0500

    Update version and author information in script

--- UserScript/PunkX Bypass.js
+++ UserScript/PunkX Bypass.js
@@ -3,23 +3,16 @@
 // @namespace    punkX-bypass-exclusive
 
 
-// @version      1.3
+// @version      2.0
 // @description  Automatische Key-Kopierung ohne Klick-System
-// @author       Mw_Anonymous | TheRealBanHammer
+// @author       FloppHub Team| Swagger Boyz
 
 // @icon         https://flopphub-team.github.io/UserScript/Rip-Pandadevelopment-Lol.jpg
 // @updateURL    https://flopphub-team.github.io/UserScript/PunkX%20Bypass.js
 // @downloadURL  https://flopphub-team.github.io/UserScript/PunkX%20Bypass.js
 
 
-// @match        https://pandadevelopment.net/getkey?*
-// @match        https://pandadev.net/getkey?*
-// @match        https://linkvertise.com/*
-// @match        https://short-jambo.com/*
-// @match        https://loot-link.com/s?*
-// @match        https://loot-links.com/s?*
-// @match        https://lootlink.org/s?*
-// @match        https://lootlinks.co/s?*
+// @match        **://pandadevelopment.net/**
 
 
 // @grant        GM_xmlhttpRequest
@@ -28,8 +21,6 @@
 
 
 // @connect      *
-
-
 // @run-at       document-start
 // ==/UserScript==
 
