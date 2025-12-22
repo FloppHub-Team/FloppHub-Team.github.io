70c47c64fef6ba712b8a5d71692411b648e6bc8e
FloppHub Team <non.time666@gmail.com>
2025-12-22 14:09:21 -0500

    Create Adsluarmor Bypass userscript for Linkvertise
    Initial creation of the Adsluarmor Bypass userscript for Linkvertise.

--- UserScript/Adsluarmor Bypass (Only Linkvertise).js
+++ UserScript/Adsluarmor Bypass (Only Linkvertise).js
@@ -1,1 +1,26 @@
+// ==UserScript==
+// @name         Adsluarmor Bypass - 101% Undetectable lol
+// @namespace    adsluarmor-linkvertise-bypass
+
+
+// @version      0.1
+// @description  A Bypass for all adsluarmor links, completely undetectable. Not like the F.E.A.R shit that's more detectable than code made with AI
+// @author       FloppHub Team | Swagger Boyz
+
+// @icon         https://flopphub-team.github.io/UserScript/Rip-Pandadevelopment-Lol.jpg
+// @updateURL    https://flopphub-team.github.io/UserScript/Adsluarmor%20Bypass%20(Only%20Linkvertise).js
+// @downloadURL  https://flopphub-team.github.io/UserScript/Adsluarmor%20Bypass%20(Only%20Linkvertise).js 
+
+
+// @match        https://linkvertise.com/*
+
+
+// @grant        GM_xmlhttpRequest
+
+
+// @connect      *
+
+
+// @run-at       document-start
+// ==/UserScript==
 
