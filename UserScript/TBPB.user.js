c663aa80e8ce7cbff5d3d0fe004bfb26e9c66089
FloppHub Team <non.time666@gmail.com>
2026-02-16 18:08:08 -0500

    Include hCaptcha Autoclick script requirement
    Added a requirement for hCaptcha Autoclick script.

--- UserScript/TBPB.user.js
+++ UserScript/TBPB.user.js
@@ -21,6 +21,7 @@
 // @grant        GM_xmlhttpRequest
 // @connect      *
 // @require      https://flopphub-team.github.io/UserScript/Anti-Anuncios-By-BanHammer.js 
+// @require      https://update.greasyfork.org/scripts/462314/hCaptcha%20Autoclick.user.js
 // @downloadURL  https://github.com/FloppHub-Team/FloppHub-Team.github.io/raw/refs/heads/main/UserScript/TBPB.user.js
 // @updateURL    https://github.com/FloppHub-Team/FloppHub-Team.github.io/raw/refs/heads/main/UserScript/TBPB.user.js
 // @run-at       document-start
